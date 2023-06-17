d9139a91263e5cd3c6608b6aec398bee30f8a2aa
isaac-svg <isaacsakyi009@gmail.com>
2023-06-17 09:11:13 -0700

    first c program in bash

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,9 @@
 /*
  * This function prints a message to the stdout using puts
  */
-int main (void)
-   {
-	    
-	puts("\"Programming is like building a multilingual puzzle");
-  	return (0);
+int main(void)
+	{    
+		puts("\"Programming is like building a multilingual puzzle");
+  		return (0);
 	
-    }
+	}
