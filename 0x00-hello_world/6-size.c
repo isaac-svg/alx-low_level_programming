7d2cf7f5c790a7a0cb2c6889e3a683dea4679b57
isaac-svg <isaacsakyi009@gmail.com>
2023-06-17 09:47:11 -0700

    size of operator

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ (*
+ * Description: prints to the out stream
+ * Return: 0 for succes and any other value for failure
+ */
+int main(void)
+	{
+		printf("Size of a char: %d byte(s)\n",sizeof(char));
+		printf("Size of an int: %d byte(s)\n",sizeof(int));
+		printf("Size of a long int : %d byte(s)\n",sizeof(long int));
+		printf("Size of a long long: %d byte(s)\n",sizeof(long long int));
+		printf("Size of a float: %d byte(s)\n",sizeof(float));
+		return (0);
+	}
