07bc9916f5867fdc0e28f12969a13f9fe6d03b60
isaac-svg <isaacsakyi009@gmail.com>
2023-06-17 09:21:56 -0700

    first c program in bash

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-/*
+/**
  * This function prints a message to the stdout using puts
  *
- * main- Entry point
+ * main - Entry point
  * Returns 0 for succes and any other value for failure
  */
 int main(void)
