e948bb316f83a21c6193f2eba265d4ee24afcde3
isaac-svg <isaacsakyi009@gmail.com>
2023-06-17 09:31:15 -0700

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ (*
+ * Description: prints to the out stream
+ * Return: 0 for succes and any other value for failure
+ */
+int main(void)
+	{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+	}
