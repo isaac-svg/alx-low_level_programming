a2a307c1f4bf47b02ae13faa5940d9fa91008214
isaac-svg <isaacsakyi009@gmail.com>
2023-06-17 10:37:51 -0700

    printing to the standead error stream

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 
 /**
  * main - Entry point
- (* * Description: prints to the out stream 
- * Return: 0 for succes and any other value for failure 
- */
+ (* * Description: prints to the out stream
+ * Return: 0 for succes and any other value for failure
+  */
 int main(void)
 	{
 		fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	       	return (1);
+		return (1);
 	}
