c9118c1a6b86f5125121eb85fca49118ad34679d
isaac-svg <isaacsakyi009@gmail.com>
2023-06-17 08:58:23 -0700

    first c program in bash

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-
+#include <stdio.h>
 
 int main(void)
     {
-	    char msg[] = "Programming is like building a multilingual puzzle";
+	    char msg[] = "\"Programming is like building a multilingual puzzle";
 	puts(msg);
     return 0;
     }
