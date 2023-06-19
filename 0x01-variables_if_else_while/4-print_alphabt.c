a2716a54a5bd3644167219a5353f61cc8cf9e6e0
isaac-svg <isaacsakyi009@gmail.com>
2023-06-19 15:09:03 -0700

    filter

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point to function
+ *
+ * Return: Always 0 if no error
+ */
+int main(void)
+{
+
+	char str[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		if (str[i] != 'q' || str[i] != 'e')
+		{	
+			putchar(str[i] | ' ');
+		}
+	}
+	
+	putchar('\n');
+	return (0);
+}
