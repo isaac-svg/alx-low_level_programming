af516e544b1294cf248737cbbf77e2e377160eae
isaac-svg <isaacsakyi009@gmail.com>
2023-06-19 12:17:16 -0700

    coverting vharacters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point to function
+ *
+ * Return: Always 0 if no error
+ */
+int main(void)
+{
+
+	char str[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(str[i] | ' ');
+	}
+	for (i = 0; i < 26; i++)
+	{
+		putchar(str[i] & '_ ');
+	}
+	putchar('\n');
+	return (0);
+}
