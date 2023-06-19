50eda1865c057d7365549e927429b3daf7b95baa
isaac-svg <isaacsakyi009@gmail.com>
2023-06-19 16:00:25 -0700

    print hex numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point to function
+ *
+ * Return: Always 0 if no error
+ */
+int main(void)
+{
+
+	char str[16] = "01234567890abcdef";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+	return (0);
+}
