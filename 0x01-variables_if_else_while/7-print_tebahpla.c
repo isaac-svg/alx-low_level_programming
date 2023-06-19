d72ee53dfdef8606f1025151d5ad8f9aef694e3e
isaac-svg <isaacsakyi009@gmail.com>
2023-06-19 15:53:11 -0700

    reverse characters

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point to function
+ *
+ * Return: Always 0 if no error
+ */
+int main(void)
+{
+
+	char str[26] = "zyxwvutsrqponmlkjihgfedcba;
+	q"nt i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(str[i] | ' ');
+	}
+	putchar('\n');
+	return (0);
+}
