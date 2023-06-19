5a35c26e8137e42a1a627b9e74d4084db0fe4d78
isaac-svg <isaacsakyi009@gmail.com>
2023-06-19 15:28:29 -0700

    count

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point to function
+ *
+ * Return: Always 0 if no error
+ */
+int main(void)
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}	
+	putchar('\n');
+	return (0);
+}
