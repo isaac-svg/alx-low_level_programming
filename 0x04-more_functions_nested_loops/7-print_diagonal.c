6e4f48d4c03d4911bbd9cbe892aec5bc520128b8
isaac-svg <isaacsakyi009@gmail.com>
2023-06-22 16:39:59 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.c"
+
+/**
+ * print_diagonal- prints a diagonal line.
+ *
+ * @n: number of lines
+ * Return:void
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		int j;
+
+		for (j = 0; j < n; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\');
+	}
+}
