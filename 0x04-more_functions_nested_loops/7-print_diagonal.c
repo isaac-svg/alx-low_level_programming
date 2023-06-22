2abe52e6ac833e8c4e469956d0152bb4165ad9e5
isaac-svg <isaacsakyi009@gmail.com>
2023-06-22 16:50:51 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,15 +9,23 @@
 void print_diagonal(int n)
 {
 	int i;
-
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
+		_putchar('\n');
+	}
+	else
+	{
+
+		for (i = 0; i < n; i++)
+		{
 		int j;
 
 		for (j = 0; j < n; j++)
 		{
 			_putchar(' ');
 		}
-		_putchar('\');
+		_putchar('\\');
+		_putchar('\n');
+		}
 	}
 }
