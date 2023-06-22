c229895ec086b7a914e7743ba15df60c90ff4b66
isaac-svg <isaacsakyi009@gmail.com>
2023-06-22 15:54:15 -0700

    Changes

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,16 +6,15 @@
 void print_most_numbers(void)
 {
 	int i;
-	fot (i = 48; i < 58; i++)
+	for (i = 48; i < 58; i++)
 	{
-		if (i != 50)
+		if (i != 52)
 		{
-			if (i != 52)
+			if (i != 50)
 			{
-
 				_putchar(i);
 			}
 		}
 	}
-	_putchar('/n');
-}
+	_putchar('\n');
+} 
