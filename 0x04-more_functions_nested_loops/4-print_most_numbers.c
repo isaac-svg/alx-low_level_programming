ed344eacc575a35dc646594ae867a69203a4d38b
isaac-svg <isaacsakyi009@gmail.com>
2023-06-22 15:22:02 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints digits without 2 and 4.
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+	fot (int i = 48; i < 58; i++)
+		if (i == 50 || i == 52)
+			continue;
+		_putchar(i);
+}
