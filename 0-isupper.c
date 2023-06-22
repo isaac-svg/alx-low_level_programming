a3da8e2192b9aa25daddcfcdff0a0ffe42ea4b80
isaac-svg <isaacsakyi009@gmail.com>
2023-06-22 14:22:33 -0700

    More functions

--- 0-isupper.c
+++ 0-isupper.c
@@ -16,3 +16,18 @@ int _isupper(char c)
 	else
 		return (0);
 }
+/**
+ * main - test code .
+*
+ * Return: 0 .
+ */
+int main(void)
+{
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
+}
