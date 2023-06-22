cb0679f2c8d4c435a6a0d0f005b9214224ae5b10
isaac-svg <isaacsakyi009@gmail.com>
2023-06-22 14:25:37 -0700

    more functions

--- 0-isupper.c
+++ 0-isupper.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include<stdio.h>
-/*
+/**
  *  _isupper - Checks if input is uppercase or lowercase
  *
+ *@c: Argument
  *  Return: 1 if if input is upercase otherwise 0
  */
 
-int _isupper(char c)
+int _isupper(int c)
 {
 	int castInt = c;
 	if ( castInt >= 65 && castInt <= 90)
