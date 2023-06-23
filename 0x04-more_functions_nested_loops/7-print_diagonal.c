fa555dae31d6df7218e24693533d17c61a14d467
isaac-svg <isaacsakyi009@gmail.com>
2023-06-23 02:59:54 -0700

    loop invariant bug

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,6 +5,7 @@
  *
  * @n: number of lines
  * Return:void
+ *
  */
 void print_diagonal(int n)
 {
@@ -20,7 +21,7 @@ void print_diagonal(int n)
 		{
 		int j;
 
-			for (j = 0; j < i; j++)
+			for (j = 1; j < i; j++)
 			{
 				_putchar(' ');
 			}
