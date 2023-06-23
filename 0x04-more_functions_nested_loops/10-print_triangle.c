2d94f27758203c977b2fb99dadf45831bdae9223
isaac-svg <isaacsakyi009@gmail.com>
2023-06-23 01:19:51 -0700

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,9 +10,9 @@ void print_triangle(int size)
 {
 	int i,j,k;
 
-	for(i = 1; i<= size; i++)
+	for(i = 1; i <= size; i++)
 	{
-		for( j = 1; j <= size; j++)
+		for( j = i; j <= size; j++)
 		{
 			_putchar(' ');
 		}
