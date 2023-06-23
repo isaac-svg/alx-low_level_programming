3e9e5e8d2e7e1647bbe503b73e5a71bba9c69508
isaac-svg <isaacsakyi009@gmail.com>
2023-06-23 00:50:30 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,24 +9,23 @@
 int main()
 {
 	int i;
-	for(i = 0; i < 100; i++)
+	for(i = 1; i <= 100; i++)
 	{
-		if(i % 3 ==0)
+		if(i % 3 == 0 && i % 5 != 0)
 		{
-			puts("Fizz");
+			puts(" Fizz");
 		}
-		else if(i % 5 == 0)
+		else if(i % 5 == 0 && i % 3 != 0)
 		{
-			puts("Buzz");
+			puts(" Buzz");
 		}
 		else if(i % 15 == 0)
 		{
-			puts("FizzBuzz");
+			puts(" FizzBuzz");
 		}
 		else
 		{
-			printf("%d", i);
+			printf(" %d", i);
 		}
-		printf("%c", ' ');
 	}
 
