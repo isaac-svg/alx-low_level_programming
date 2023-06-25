9f8087393cc3174b3a5748137df0b6b3ff22c020
isaac-svg <isaacsakyi009@gmail.com>
2023-06-25 02:01:36 -0700

    10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
- *  print_alphabet_x10 - prints the lowercase alphabets 10 times followed by a new line
+ *  print_alphabet_x10 - prints the lowercase alphabets 10
+ *  times followed by a new line
  *  Return: void
  */
 void print_alphabet_x10(void)
 {
-	int i,j;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 97; j < 123; j++)
 		{
 
-		_putchar(i);
+		_putchar(j);
 		}
-		_putchar('\n');
+		_putchar('\n')
 	}
 	
 }
