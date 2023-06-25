90a49c4392ac974f9f4810009190509721455b3b
isaac-svg <isaacsakyi009@gmail.com>
2023-06-25 06:12:46 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,12 +6,15 @@
  * @n: number to print from untiil 98
  * Return: void
  */
-void print_to_98(int n){
+void print_to_98(int n)
+{
 	int i;
-	
 	for (i = n; i <= 98;)
 	{
-		printf("%d, ", i);
+		if (i == 98)
+			printf(%d", i);
+		else
+	 		printf("%d, ", i);
 		if (n <= 98)
 			i++;
 		else
