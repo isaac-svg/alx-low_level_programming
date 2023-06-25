fad30a964c9fff827aa6f09378fe4a4624a4f66d
isaac-svg <isaacsakyi009@gmail.com>
2023-06-25 07:04:58 -0700

    sum

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,7 @@
 
 /**
  * main - entyr poin t to program
- * Return sum of the mutiples of 3 and 5 bellow 1024
+ * Return: sum of the mutiples of 3 and 5 bellow 1024
  */
 int main(void)
 {
@@ -13,9 +13,7 @@ int main(void)
 	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 3 == 0) || (i % 5 == 0))
-			sum += sum;
+			sum += i;
 	}
 	return (sum);
 }
-
-
