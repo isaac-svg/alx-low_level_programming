ce6cc756fdcbfaca9e110418bdb5c93906f68135
isaac-svg <isaacsakyi009@gmail.com>
2023-06-25 06:43:48 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,38 +8,37 @@
  */
 void print_to_98(int n)
 {
-	int i;
-		if (n > 98)
+	if (n > 98)
 	{
-		for (int i = n; i > 98; i++)
+		int i;
+		for ( i = n; i < 98; i--)
 		{
-		if (i == 98)
-		{
-			printf("%d\n",n);
-			/* code */
+			if (i == 98)
+			{
+				printf("%d\n", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
+		
 		}
-		else
+	
+	}
+	else
+	{
+		int i;
+		for ( i = n; i < 98; i++)
 		{
-		printf("%d, ", i);
-		}
-
-			i--;
-
+			if (i == 98)
+			{
+				printf("%d\n", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
+		
 		}
 	}
-		else
-		{
-
-	for (i = n; i <= 98;)
-	{
-		if (i == 98)
-			printf("%d\n", i);
-		else
-	 		printf("%d, ", i);
-		if (n <= 98)
-			i++;
-		else
-			i--;
-	}
-		}
 }
