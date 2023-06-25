7fd0e50fa285dc9eda0f364974277775385cdf93
isaac-svg <isaacsakyi009@gmail.com>
2023-06-25 03:32:51 -0700

    print hour and minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints the minutes and hours 
+ * Retun: void
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	hour = 0;
+	while (hour < 24)
+	{
+		minute = 0;
+		while (minute < 60)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			minute++;
+		}
+		hour++;
+	}
+}
