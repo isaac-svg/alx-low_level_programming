ece32a9e44cf7e2f55b6976d061f8b6a8edd3de5
isaac-svg <isaacsakyi009@gmail.com>
2023-06-25 02:18:32 -0700

    alphabetic function

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *  _isalpha - checks if the character argument is
+ *  alphabetic character
+ *  @c: input parameter
+ *  Return: 1 if true otherwise 0
+ */
+int _isalpha(int c)
+{
+	returnn (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z');
+}
