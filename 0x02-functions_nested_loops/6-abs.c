0bb3e9c0666cfc131ef3032f37264da7516c85de
isaac-svg <isaacsakyi009@gmail.com>
2023-06-25 02:45:25 -0700

    absolute of anumber

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute of the argument
+ * @n: integer parameter
+ * Return: absolute of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
