9ec90dbe18ee5e5512d2821ba0ac258124d448cc
isaac-svg <isaacsakyi009@gmail.com>
2023-06-26 17:48:11 -0700

    reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,17 +6,15 @@
  */
 void rev_string(char *s)
 {
-	int len;
+	int len, temp;
 
 	len = 0;
 	while (*(s + len) != '\0')
 	{
 		++len;
 	}
-	int temp;
-
 	temp = 0;
-	while (temp < (leng / 2))
+	while (temp < (len / 2))
 	{
 		s[temp] = s[len - temp];
 		++temp;
