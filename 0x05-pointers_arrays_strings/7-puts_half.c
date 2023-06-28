3fa4d5052353248663c172c85947ae374fca402c
isaac-svg <isaacsakyi009@gmail.com>
2023-06-28 00:02:28 +0000

    print half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts_half - prints characters to length / 2
+ * @str: input stting
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len, i, j;
+
+	len = 0;
+	while (*(str + len) != '\n')
+		++len;
+	i = len % 2;
+	if (i == 0)
+	{
+		for (j = (len / 2); j < len; j++)
+		{
+			_putchar(*(str + j));
+		}
+	}
+	else
+	{
+		for (j = ((len - 1) / 2); j < len; j++)
+		{
+			_putchar(*(str + j));
+		}
+	}
+	_putchar('\n');
+}
