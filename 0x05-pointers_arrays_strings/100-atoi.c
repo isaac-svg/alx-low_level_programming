d2b820189544a3936b9b6ed8a00f409b8490a83d
isaac-svg <isaacsakyi009@gmail.com>
2023-06-28 02:52:16 +0000

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,26 +2,28 @@
 /**
  * _atoi - functuons same as the built in atoi
  * @s: input string
- * Return: 0 if the string has no int else 
+ * Return: 0 if the string has no int else
  * the digit is returned
  */
 int _atoi(char *s)
 {
-	int result, len, diglen, sign, hasdigit;
+	int result, len, diglen, found, sign, hasdigit;
 
 	len = 0;
 	sign = 1;
 	result = 0;
+	found = 0;
 	while (*(s + len) != '\0')
 	{
 		if (*(s + len) >= '0' && *(s + len) <= '9')
 		{
 			if (*(s + len - 1) == '-')
-			{	
+			{
 				sign = -1;
 			}
 			hasdigit = 1;
 			diglen = 0;
+			found = 1;
 			while (*(s + diglen + len) >= '0' && *(s + diglen + len) <= '9')
 			{
 				result = (result * 10) + (*(s + diglen + len) - '0');
@@ -29,6 +31,8 @@ int _atoi(char *s)
 			}
 			len += diglen;
 		}
+		if (found)
+			break;
 		++len;
 	}
 	if (!hasdigit)
