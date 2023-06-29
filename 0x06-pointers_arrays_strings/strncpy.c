df83c9671c53e56f89958139ca4123e94ff0b719
isaac-svg <isaacsakyi009@gmail.com>
2023-06-29 23:54:48 +0000

    strncpy

---
+++ 0x06-pointers_arrays_strings/strncpy.c
@@ -0,0 +1,25 @@
+/**
+ * _strncpy - works same as strncpy
+ * @dest: dsstination string
+ * @src: source string
+ * Return: pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len, i, src_len;
+
+	src_len = 0;
+	while (*(src + src_len) != '\0')
+	{
+		++src_len;
+	}
+	for (i = 0; i < n; i++)
+	{
+		if (i >= src_len)
+			*(dest + i) = '\0';
+		else
+			*(dest + i) = *(src + i);
+	}
+
+	return (dest);
+}
