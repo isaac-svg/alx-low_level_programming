cae084189d82d25126018519a65a0d445114794d
isaac-svg <isaacsakyi009@gmail.com>
2023-06-29 22:59:24 +0000

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strcat - copys one string onto another
+ * @dest: destination string
+ * @src: source string
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dest_len, src_len;
+
+	dest_len = 0;
+	src_len = 0;
+	while (*(dest + dest_len))
+	{
+		if (*(dest + dest_len) == '\0')
+		{
+			while (*(src + src_len) != '\0')
+			{
+				*(dest + dest_len + src_len) = *(src + src_len);
+				++src_len;
+				++dest_len;
+			}
+			*(dest + dest_len + 1) = '\0';
+			break;
+		}
+		++dest_len;
+	}
+}
+
