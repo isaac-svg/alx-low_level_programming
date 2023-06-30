24f6544fedf7fd7dcb035665ce0e000b4450d1fa
isaac-svg <isaacsakyi009@gmail.com>
2023-06-30 12:17:43 +0000

    correction

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,6 +2,7 @@
  * _strncpy - works same as strncpy
  * @dest: dsstination string
  * @src: source string
+ * @n: length to copy
  * Return: pointer to dest
  */
 char *_strncpy(char *dest, char *src, int n)
