1a4ee40357020a44cc78cc4e16285b028eac8460
isaac-svg <isaacsakyi009@gmail.com>
2023-06-30 11:52:38 +0000

    correction

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,24 +6,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int dest_len, src_len;
+	int dlen, slen;
 
-	dest_len = 0;
-	src_len = 0;
-	while (*(dest + dest_len))
+	dlen = 0;
+	while (dest[dlen] != '\0')
 	{
-		if (*(dest + dest_len) == '\0')
-		{
-			while (*(src + src_len) != '\0')
-			{
-				*(dest + dest_len + src_len) = *(src + src_len);
-				++src_len;
-				++dest_len;
-			}
-			*(dest + dest_len + 1) = '\0';
-			break;
-		}
-		++dest_len;
+		dlen++;
 	}
+	slen = 0;
+	while (src[slen] != '\0')
+	{
+		dest[dlen] = src[slen];
+		dlen++;
+		slen++;
+	}
+	dest[dlen] = '\0';
+
 	return (dest);
 }
