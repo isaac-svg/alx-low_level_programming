645145cc83ce4516ab9d14d7b226d157db74ee41
isaac-svg <isaacsakyi009@gmail.com>
2023-06-30 00:29:53 +0000

    strncmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+/**
+ * _strcmp - compares strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0 if same
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int s1_len, s2_len, i;
+
+	s1_len = 0;
+	s2_len = 0;
+	while (*(s1 + s1_len) != '\0')
+	{
+		++s1_len;
+	}
+	while (*(s2 + s2_len) != '\0')
+	{
+		++s2_len;
+	}
+	for (i = 0; i < s1_len; i++)
+	{
+		if (*(s1 + i) > *(s2 + i))
+			return (1);
+		else if (*(s1 + i) < *(s2 + i))
+			return (-1);
+	}
+	return (0);
+}
