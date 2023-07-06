cdc962b96464b5a88767d9e6ead1fb2f09e6e3bb
isaac-svg <isaacsakyi009@gmail.com>
2023-07-06 23:48:30 +0000

    raises a number to a power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - returns a number to a power
+ * @x: base
+ * @y: exponent
+ * Return: x^y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (n * _pow_recursion(x, (y -1));
+}
+
