f83f80fe7fc29b5638c9b79969143f60865d2edf
isaac-svg <isaacsakyi009@gmail.com>
2023-07-06 23:26:48 +0000

    computes the factorial of input argument

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include main.h
+/**
+ * factorial - computes the factorial of a number
+ * @n: input argument
+ * Return: factorial of n or -1 if n < 0
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n < 1)
+		return (1);
+	return n * factorial(n - 1);
+}
