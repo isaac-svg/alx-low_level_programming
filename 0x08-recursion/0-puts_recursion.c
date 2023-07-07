d8b9b642552c2eafaa187cda0fc28fbd4f151dcb
isaac-svg <isaacsakyi009@gmail.com>
2023-07-07 00:04:04 +0000

    get to format

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
 * _puts_recursion - prints the characters of a string
 * @s: pointer to input string
@@ -9,13 +9,25 @@ void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		_putchar(*s);
+		putchar(*s);
 		_puts_recursion(s + 1);
 	}
 	else
 	{
-		_putchar('\n');
+		putchar('\n');
 	}
 
 }
 
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _puts_recursion("Puts with recursion");
+    return (0);
+}
