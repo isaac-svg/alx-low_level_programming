19ac3050e39129b7bf00c5cd2b8270a0b7a65422
isaac-svg <isaacsakyi009@gmail.com>
2023-07-07 00:55:36 +0000

    merge conflict resolution

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+int get_prime(int num, int counter);
+
+/**
+ * is_prime_number - checks if a number is prime or not
+ * @n: input argument
+ @description: helper function
+ * Return: 1 if number is prime else 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (get_prime(n, nu - 1));
+}
+
+/**
+ * actual_prime - calculates if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ * @description: helper function
+ * Return: 1 if n is prime, 0 if not
+ */
+int get_prime(int num, int counter)
+{
+	if (counter == 1)
+		return (1);
+	if ((num % counter == 0) && (counter > 0))
+		return (0);
+	return (get_prime(num, (counter - 1)));
+}
+
