1c72568f718b3bbe7882b78d243d0cc6afad0ccd
isaac-svg <isaacsakyi009@gmail.com>
2023-07-12 00:29:53 +0000

    malloc and free

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stlib.h>
+
+/**
+ * alloc_grid - creates a 2D array
+ * @width: row size  of the array
+ * @height: column size  of the array
+ * Return: pointer to the 2D array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **arr, i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	arr = malloc(sizeof(int) * (width * height));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+	return (arr);
+}
