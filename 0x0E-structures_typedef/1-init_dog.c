a767119c7bb56c1014eea03d317f31cb4636c2e0
isaac-svg <isaacsakyi009@gmail.com>
2023-07-17 23:16:34 +0000

    structures and typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+/**
+ * init_dog - initialises the members of dog
+ * @d: pointer to dog
+ * @name: name of dog passed as argument
+ * @age: age of dog passed as argument
+ * @owner: name of owner passed as argument
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
