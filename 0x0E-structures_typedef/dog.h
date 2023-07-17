7064dc4a8c1f7933168ce389a177d2b5e3d2afa0
isaac-svg <isaacsakyi009@gmail.com>
2023-07-17 22:59:26 +0000

    structures and typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure for a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
