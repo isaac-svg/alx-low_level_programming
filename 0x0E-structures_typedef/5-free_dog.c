614d9319d4bb803f2b83412a02e7cce4e49f967b
isaac-svg <isaacsakyi009@gmail.com>
2023-07-18 00:55:16 +0000

    structures and typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees memory allocated to a struct dog
+ * @d: pointer to dog struct 
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
+
