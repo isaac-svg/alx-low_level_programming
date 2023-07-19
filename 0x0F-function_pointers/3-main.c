55983d8780b22e50323835cc5f986fe5600f66b1
isaac-svg <isaacsakyi009@gmail.com>
2023-07-19 18:53:02 +0000

    function pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,7 +13,7 @@
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 
-	char *operator;
+	char *opt;
 	int num1, num2;
 
 	if (argc != 4)
@@ -23,23 +23,23 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	}
 
 	num1 = atoi(argv[1]);
-	op = argv[2];
+	opt = argv[2];
 	num2 = atoi(argv[3]);
 
-	if ((get_op_func(op) == NULL) || op[1] != '\0')
+	if ((get_op_func(opt) == NULL) || opt[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*op == '/' && num2 == 0) ||
-	    (*op == '%' && num2 == 0))
+	if ((*opt == '/' && num2 == 0) ||
+	    (*opt == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(op)(num1, num2));
+	printf("%d\n", get_op_func(opt)(num1, num2));
 
 	return (0);
 }
