850c9f9100c8c770aa3855f32d584001e31c4e4b
isaac-svg <isaacsakyi009@gmail.com>
2023-07-28 14:03:09 +0000

    Linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints the values of a linked list
+ * @h: pointer to the head of the list
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nill)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		num += 1;
+		h = h->next;
+	}
+
+	return (num);
+}
