9f9494d7632cddf0559f61047b1fb538e2787680
isaac-svg <isaacsakyi009@gmail.com>
2023-07-30 21:57:23 +0000

    correction

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,19 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int change_coin(int total);
+
+
+/**
+ * change_coin - prints the minimum number of coins to
+ * make change for an amount of money
+ * @total: the total amount of money to make change for
+ *
+ * Return: 0 if no error otherwise 1
+ */
+int change_coin(int total)
+{
+	int num, idx, coin_count;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (total < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	coin_count = 0;
+	num = total;
+
+	for (idx = 0; idx < 5 && num >= 0; idx++)
+	{
+		while (num >= coins[idx])
+		{
+			coin_count++;
+			num -= coins[idx];
+		}
+	}
+
+	printf("%d\n", coin_count);
+	return (0);
+}
+
 /**
  * main - prints the minimum number of coins
  * @argc: argument count
  * @argv: arguments vector
  * Return: 0 if non-error otherwise 1
  */
-
 int main(int argc, char *argv[])
 {
 	int num;
-        int result;
-        int i;
-
-	int coins[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
 	{
@@ -22,21 +55,9 @@ int main(int argc, char *argv[])
 	}
 
 	num = atoi(argv[1]);
-	result = 0;
 
-	if (num < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-	for (i = 0; (i < 5 && num >= 0); i++)
-	{
-		while (num >= coins[i])
-		{
-			result++;
-			num -= coins[i];
-		}
-	}
-	printf("%d\n", result);
+	change_coin(num);
+
 	return (0);
 }
+
