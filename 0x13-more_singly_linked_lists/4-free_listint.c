7e57bd4e23230117239b390f7b2aea9e55ef97d8
isaac-svg <isaacsakyi009@gmail.com>
2023-08-01 23:56:58 +0000

    More Linked List

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint - frees a linked list
+ * @head: pointer to the head
+ *
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t temp;
+
+	while (head)
+	{
+		temp = head;
+		head = temp->next;
+		free(temp);
+	}
+}
