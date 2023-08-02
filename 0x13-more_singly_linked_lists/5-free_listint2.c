f9e4e8131cf528c97cb4bc55e38d5fd746ce8482
isaac-svg <isaacsakyi009@gmail.com>
2023-08-02 00:16:50 +0000

    More Singly Linked List

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,15 +11,16 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *current, *temp;
+
 	if (head == NULL)
 		return;
 	current = *head;
 	while (current)
 	{
 		temp = current;
-        	current = current->next;
-        	free(temp);
-    	}
+		current = current->next;
+		free(temp);
+	}
 
-    	*head = NULL;
+	*head = NULL;
 }
