8f83d6509a942f7494fc177f85b7919a80ce56a5
isaac-svg <isaacsakyi009@gmail.com>
2023-08-02 01:30:34 +0000

    More Linked List

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *node, *currNode, *nxtNode;
-	unsigned int count;
+	unsigned int count = 0;
 
 	node = malloc(sizeof(listint_t));
 	if (node == NULL)
