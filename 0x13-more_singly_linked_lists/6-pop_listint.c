ad2811a53c5b89812b0a6b56569f10410b2accb1
isaac-svg <isaacsakyi009@gmail.com>
2023-08-02 00:27:27 +0000

    More Linked List

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * pop_listint - deletes the head of a linked list
+ * @heaad: pointer to the head
+ *
+ * Return: head value
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp = *head;
+	int val;
+
+	if(temp == NULL)
+		return (0);
+	/* delete head opreation */
+
+	*head = (*head)->next;
+	val = temp->n;
+	free(temp);
+	return (val);
+}
+
