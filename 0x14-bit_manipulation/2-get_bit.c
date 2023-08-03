f92bb31aad2e9f934a0cddc5c3e4717dda3ad1ae
isaac-svg <isaacsakyi009@gmail.com>
2023-08-03 18:17:39 +0000

    Bit Manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,9 +11,9 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
-		return -1;
+		return (-1);
 
 	unsigned long int mask = 1UL << index;
 
-	return (n & mask) ? 1 : 0;
+	return ((n & mask) ? (1) : (0));
 }
