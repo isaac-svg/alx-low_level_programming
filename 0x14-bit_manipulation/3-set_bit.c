37a496b21e4c4aaf14caa84f9bf80bca3405cfa6
isaac-svg <isaacsakyi009@gmail.com>
2023-08-03 18:49:14 +0000

    Bit Maniplation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,10 +9,12 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int mask = 1UL;
+
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	unsigned long int mask = 1UL << index;
+	mask = 1UL << index;
 
 	*n |= mask;
 
