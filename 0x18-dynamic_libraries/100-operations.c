e20a3c8352ef64b2b9522882c8f7c57d6c0656db
isaac-svg <isaacsakyi009@gmail.com>
2023-10-16 21:35:52 +0000

    shared libraries

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,58 @@
+/**
+  * add - adds to numbers
+  * @a: first number
+  * @b: second number
+  * Return: a + b
+  */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+  * mul - multiplies two numbers
+  * @a: first number
+  * @b: second number
+  * Return: a * b
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+  * div - divides two numbers
+  * @a: first number
+  * @b: second number
+  * Return: a / b
+  */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+  * sub - subtructs two numbers
+  * @a: first number
+  * @b: second number
+  * Return: a + b
+  */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+  * mod - takes the mode of two numbers
+  * @a: first number
+  * @b: second number
+  * Return: a % b
+  */
+int mod(int a, int b)
+{
+	return (a % b);
+}
