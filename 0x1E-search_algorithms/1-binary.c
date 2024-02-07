8a5bbd2592be081141ff4417f3148610f44931fd
isaac-svg <isaacsakyi009@gmail.com>
2024-02-07 01:06:23 -0500

    Binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,5 +1,39 @@
 #include "search_algos.h"
 
+
+
+
+
+
+
+/**
+ * print_array - Prints the elements of an array
+ * @array: The array to print
+ * @low: The index to start print from
+ * @high: the Index to end printing
+ * Return: void
+ */
+void print_array(int *array, size_t low, size_t high)
+{
+	size_t j;
+
+	printf("Searching in array:");
+
+	for (j = low; j < high; j++)
+	{
+		if (j == low)
+		{
+			printf(" %d", array[j]);
+		}
+		else
+		{
+			printf(" ,%d",array[j]);
+		}
+	}
+
+	printf("\n");
+}
+
 /**
  * binary_search - performs a biney search for an element
  * in an array
